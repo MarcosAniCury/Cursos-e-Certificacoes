29d136b97767c88c2cda46c55a854a8fae4e3e55
Marcos Ani Cury Vinagre Silva <65677865+MarcosAniCury@users.noreply.github.com>
2021-07-29 17:47:11 -0300

    Comeco do curso de orientacao a objeto

---
+++ php-Orientacao a objeto/src/Conta.php
@@ -0,0 +1,19 @@
+<?php
+
+class Conta 
+{
+
+    public string $cpf;
+    public string $nome;
+    public float $saldo;
+
+    /*function criarConta(string $cpf, string $nomeTitular, float $saldo): array
+    {
+        return [
+            $cpf => [
+                'titular' => $nomeTitular,
+                'saldo' => $saldo
+            ]
+        ];
+    }*/
+}
