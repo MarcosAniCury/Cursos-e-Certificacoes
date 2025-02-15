e5d96ea2d5e7bba04a42c8c33c89f1f642056d3d
MarcosAniCury <curymarcos@live.com>
2025-02-15 18:52:26 -0300

    Feat: add monitor logic to check websites status code

--- Go-a linguagem do Google/MonitorSite/main.go
+++ Go-a linguagem do Google/MonitorSite/main.go
@@ -1,14 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
-func main() {
+var SITES_TO_MONITOR = []string{"https://www.sofex.com.br", "https://www.facialmap.com.br"}
+
+func monitoringWebsite() {
+	fmt.Println("Initialize monitor...")
+	for {
+		for _, site := range SITES_TO_MONITOR {
+			fmt.Println("Check website: ", site, "")
+			result, err := http.Get(site)
+
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+
+			if result.StatusCode == 200 {
+				fmt.Println("Site is OK (200)...")
+			} else {
+				fmt.Println("Error: Status Code ", result.StatusCode)
+			}
+		}
+	}
+}
+
+func showMenu() int {
+	var command int
 	fmt.Println("This software monitoring you web site, Welcome")
 	fmt.Println("1 - Start Monitoring")
 	fmt.Println("2 - Show Logs")
 	fmt.Println("0 - Exit")
 
-	var command int
 	fmt.Print("Enter your command: ")
 	fmt.Scanln(&command)
+
+	switch command {
+	case 1:
+		monitoringWebsite()
+	case 2:
+		fmt.Println("Logs will be show...")
+	case 0:
+		fmt.Println("Exit aplication...")
+		os.Exit(0)
+	default:
+		fmt.Println("Command not recognize, insert again")
+		showMenu()
+	}
+	return command
+}
+
+func main() {
+	showMenu()
 }
