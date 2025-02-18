a2a0cbcae4502111fda1b8024edc66af9bd5e34f
MarcosAniCury <curymarcos@live.com>
2025-02-17 21:29:22 -0300

    Feat: finish course and monitor website program

--- Go-a linguagem do Google/MonitorSite/main.go
+++ Go-a linguagem do Google/MonitorSite/main.go
@@ -1,35 +1,94 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
-var SITES_TO_MONITOR = []string{"https://www.sofex.com.br", "https://www.facialmap.com.br"}
-
+const ARQUIVE_WITH_SITES = "sites-to-monitoring.txt"
+const ARQUIVE_TO_WRITE_LOGS = "sites-status-log.txt"
 const WAIT_TIME_BEFORE_MONITOR_AGAIN = time.Minute * 10
+
+func writeArquiveLn(arquiveName string, text string) {
+	arq, error := os.OpenFile(arquiveName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if error != nil {
+		fmt.Println("Erro happend here:", error)
+		os.Exit(-1)
+	}
+
+	arq.WriteString(text)
+
+	arq.Close()
+}
+
+func readArquiveLn(arquiveName string) []string {
+	arq, error := os.Open(arquiveName)
+
+	if error != nil {
+		fmt.Println("Erro happend here:", error)
+		os.Exit(-1)
+	}
+
+	var lines []string
+
+	reader := bufio.NewReader(arq)
+
+	for {
+		line, error := reader.ReadString('\n')
+
+		lines = append(lines, strings.TrimSpace(line))
+
+		if error == io.EOF {
+			break
+		}
+	}
+
+	arq.Close()
+
+	return lines
+}
 
 func monitoringWebsite() {
 	fmt.Println("Initialize monitor...")
 	for {
-		for _, site := range SITES_TO_MONITOR {
+		for _, site := range readArquiveLn(ARQUIVE_WITH_SITES) {
 			fmt.Println("Check website: ", site, "")
 			result, err := http.Get(site)
 
 			if err != nil {
 				fmt.Println("Error:", err)
+				os.Exit(-1)
 			}
 
+			var logWrite string
+
+			dateNow := time.Now().Format("02/01/2006 15:04:05")
+
 			if result.StatusCode == 200 {
+				logWrite = fmt.Sprintf("%s: %s is OK (200)...\n", dateNow, site)
 				fmt.Println("Site is OK (200)...")
 			} else {
+				logWrite = fmt.Sprintf("%s: %s Error: Status Code %d", dateNow, site, result.StatusCode)
 				fmt.Println("Error: Status Code ", result.StatusCode)
 			}
+			writeArquiveLn(ARQUIVE_TO_WRITE_LOGS, logWrite)
 		}
 		time.Sleep(WAIT_TIME_BEFORE_MONITOR_AGAIN)
 	}
+}
+
+func readLog() int {
+	fmt.Println("Logs will be show...")
+	for _, log := range readArquiveLn(ARQUIVE_TO_WRITE_LOGS) {
+		fmt.Println(log)
+	}
+	return 1
 }
 
 func showMenu() int {
@@ -46,7 +105,7 @@ func showMenu() int {
 	case 1:
 		monitoringWebsite()
 	case 2:
-		fmt.Println("Logs will be show...")
+		readLog()
 	case 0:
 		fmt.Println("Exit aplication...")
 		os.Exit(0)
